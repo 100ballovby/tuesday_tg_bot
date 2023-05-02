3450d368fd7740feb7718a27fea35498ca6099d3
greatraksin <greatraksin@icloud.com>
2023-05-02 17:19:35 +0300

    Adding new functions for dog and meme

--- functions.py
+++ functions.py
@@ -1,8 +1,23 @@
 from jinja2 import Template
+import requests as r
+import os
+import random
 
 
 def make_template(filename):
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         template_text = f.read()
     template = Template(template_text)
     return template
+
+
+def send_image():
+    content = r.get('https://random.dog/woof.json').json()
+    img_url = content['url']
+    return img_url
+
+
+def send_meme():
+    response = r.get('https://api.imgflip.com/get_memes').json()
+    r_meme = random.choice(response['data']['memes'])
+    return r_meme['url']
--- main.py
+++ main.py
@@ -10,7 +10,27 @@ def hello_message(message):
     template = functions.make_template('templates/start.html')
     username = message.chat.username
     msg_text = template.render(name=username)
+    pin = bot.send_message(message.chat.id, text=msg_text, parse_mode='html')  # сохраняем сообщение в переменной
+    bot.pin_chat_message(message.chat.id, message_id=pin.id)  # закрепляем в чате сообщение бота
+
+
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    template = functions.make_template('templates/help.html')
+    msg_text = template.render()
     bot.send_message(message.chat.id, text=msg_text, parse_mode='html')
+
+
+@bot.message_handler(commands=['dog'])
+def send_dog(message):
+    img = functions.send_image()
+    bot.send_photo(message.chat.id, photo=img)
+
+
+@bot.message_handler(commands=['meme'])
+def send_meme(message):
+    img = functions.send_meme()
+    bot.send_photo(message.chat.id, photo=img)
 
 
 if __name__ == '__main__':
